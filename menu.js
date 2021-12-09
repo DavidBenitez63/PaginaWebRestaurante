9dde4259f5c6f2f4ebec704d4c861cf89ab2bf6f
DavidBenitez63 <david.63bg@gmail.com>
2021-12-09 01:41:57 -0600

    Carga del index v0.4

--- menu.js
+++ menu.js
@@ -1,3 +1,4 @@
+/* Esta parte el codigo es para al darle click al icono de menu, se nos desplacen las opciones*/
 var btnmenu =document.getElementById('btn-menu');
 var nav =document.getElementById('nav');
 
