2fcd08df0e62476350bfc52ae04795483a5dfe97
DavidBenitez63 <david.63bg@gmail.com>
2021-12-10 06:33:45 -0600

    Carga del index v1.0(final)

--- menu.js
+++ menu.js
@@ -1,8 +1,8 @@
 /* Esta parte el codigo es para al darle click al icono de menu, se nos desplacen las opciones*/
-var btnmenu =document.getElementById('btn-menu');
-var nav =document.getElementById('nav');
+var btnmenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
 
-btnmenu.addEventListener('click',function() {
+btnmenu.addEventListener('click', function() {
   nav.classList.toggle('muestrate')
 
 })
